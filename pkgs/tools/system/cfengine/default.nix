fd7586c642e033baa86e069931010d796dd84862
Diego Zamboni <diego@zzamboni.org>
2017-09-22 22:44:19 +0200

    Trying to fix dependencies

--- pkgs/tools/system/cfengine/default.nix
+++ pkgs/tools/system/cfengine/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
     sha256 = "d4fa9ac7276dba7b85d6757aab2f0929ab8d3b115cb0e7b0cf984760347429d7";
   };
   
-  nativeBuildInputs = [ pcre openssl lmdb ];
+  buildInputs = [ pcre openssl lmdb ];
 
   meta = with stdenv.lib; {
     description = "Help manage and understand IT infrastructure";
